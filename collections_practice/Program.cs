137f2ae9aea130ca49789cdfc6edf631d5e29298
Shovit Kandel <shovit.kandel@gmail.com>
2017-11-13 14:03:49 -0600

    update assignment

--- collections_practice/Program.cs
+++ collections_practice/Program.cs
@@ -7,7 +7,7 @@ namespace collections_practice
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            
             int[] num ={0,1,2,3,4,5,6,7,8,9};
             string[] names = {"Tim", "Martin", "Nikki", "Sara"};
             bool[] values = {true, false, true, false, true, false, true, false, true, false};
@@ -16,7 +16,7 @@ namespace collections_practice
                 Console.WriteLine(value);
             }
 
-            /*int [,] multiplication_table = new int[10,10]
+            int [,] multiplication_table = new int[10,10]
                 {
                     {1, 2, 3, 4, 5, 6, 7, 8,9, 10},
                     {2, 4, 6, 8, 10, 12, 14, 16, 18, 20},
@@ -29,8 +29,8 @@ namespace collections_practice
                     {9, 18, 27, 36, 45, 54, 63, 72, 81, 90},
                     {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
                 };
-            console.writeline(multiplication_table[0,0]);
-            */
+            Console.WriteLine(multiplication_table[0,0]);
+            
 
             //Create a List of Ice Cream flavors that holds at least 5 different flavors (feel free to add more than 5!)
             List<string> ice_cream_flavors =  new List<string>();
@@ -44,8 +44,27 @@ namespace collections_practice
             ice_cream_flavors.RemoveAt(2);
             Console.WriteLine("There are " + ice_cream_flavors.Count + " flavors in this list. ");
             
+            //Create a Dictionary that will store both string keys as well as string values
+           
+            Random rand = new Random();
+            
+            Dictionary<string,string> user = new Dictionary<string,string>();
+            foreach(var name in names){
+                user.Add(name, ice_cream_flavors[rand.Next(0,4)]);
+            }
+            /*foreach(var entry in user){
+                entry.Value = "test";
+                Console.WriteLine(entry.Value);
+                
+                //user[entry] = ice_cream_flavors[rand.Next(0,4)];
+            }
+            */
+             //Console.WriteLine(rand.Next(0,4));
+             //Console.WriteLine(rand.Next(0,4));
 
-
+            foreach(KeyValuePair<string,string> entry in user){
+                Console.WriteLine(entry.Key + " - " +  entry.Value);
+            }        
         }
     }
 }

2d52bf353238346192b4c2a37e651227425981a8
Shovit Kandel <shovit.kandel@gmail.com>
2017-11-15 19:59:08 -0600

    adding boxing and unboxing assignment

---
+++ boxing_unboxing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> myList = new List<object>();
+            myList.Add(7);
+            myList.Add(28);
+            myList.Add(-1);
+            myList.Add(true);
+            myList.Add("chair");
+
+            int sum = 0;
+            foreach(var obj in myList){
+                Console.WriteLine(obj);
+                if(obj is int){
+                sum+= (int)obj;
+                }
+            }
+            Console.WriteLine(sum);
+   
+        }
+    }
+}
